1f1a6903dd1f4b4f889463601093bb949ecf65a5
yavorant <yavorant@gmail.com>
2022-11-18 15:05:35 +0200

    map array of monsters

--- src/App.js
+++ src/App.js
@@ -7,24 +7,26 @@ class App extends Component {
     super();
 
     this.state = {
-      monster1: {
-        name: 'Linda',
-      },
-      monster2: {
-        name: 'Frank',
-      },
-      monster3: {
-        name: 'Jacky',
-      },
+      monsters: [
+        {
+          name: 'Linda',
+        },
+        {
+          name: 'Frank',
+        },
+        {
+          name: 'Jacky',
+        },
+      ],
     };
   }
 
   render() {
     return (
       <div className='App'>
-        <h1>{this.state.monster1.name}</h1>
-        <h1>{this.state.monster2.name}</h1>
-        <h1>{this.state.monster3.name}</h1>
+        {this.state.monsters.map((monster) => (
+          <h1>{monster.name}</h1>
+        ))}
       </div>
     );
   }
