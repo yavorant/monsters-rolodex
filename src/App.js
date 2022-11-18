294d47f96eecd178a7678a416db14a432bdfb830
yavorant <yavorant@gmail.com>
2022-11-18 11:24:03 +0200

    Add setState method to change the name

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Ivan',
+    };
+  }
+
   render() {
     return (
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
+          <p>Hi {this.state.name}</p>
+          <button
+            onClick={() => {
+              this.setState({ name: 'Antonio' });
+            }}
           >
-            Learn React
-          </a>
+            Change name
+          </button>
         </header>
       </div>
     );
