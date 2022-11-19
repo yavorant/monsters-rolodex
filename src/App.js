54209c121ae39fd11bd247b5b5081f3cc6a4a72b
yavorant <yavorant@gmail.com>
2022-11-19 12:23:27 +0200

    Add input with onChange, and log(event.target.value)

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
-    console.log('hi from constructor');
+    console.log('constructor');
   }
 
   componentDidMount() {
-    console.log('hi from componentDidMount');
+    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users') //promise
       .then((response) => response.json())
       .then((users) =>
@@ -29,9 +29,17 @@ class App extends Component {
   }
 
   render() {
-    console.log('hi from render');
+    console.log('render');
     return (
       <div className='App'>
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={(event) => {
+            console.log('event: ', event.target.value);
+          }}
+        />
         {this.state.monsters.map((monster) => (
           <div key={monster.id}>
             <h1>{monster.name}</h1>
