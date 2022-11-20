ade1e88a70374c5353a83720a91864e8096289a8
yavorant <yavorant@gmail.com>
2022-11-20 13:53:56 +0200

    Converting

--- src/App.js
+++ src/App.js
@@ -1,69 +1,110 @@
-import { Component } from 'react';
+// import { Component } from 'react';
+import { useState, useEffect } from 'react';
+
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
-    this.state = {
-      monsters: [],
-      searchField: '',
-    };
-
-    // console.log('constructor');
-  }
-
-  componentDidMount() {
-    // console.log('componentDidMount');
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users') //promise
       .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          }
-          // },
-          // () => {
-          //   console.log(this.state);
-          // }
-        )
-      );
-  }
+      .then((users) => setMonsters(users));
+  }, []); // [] empty array as dependency means don't watch changes, load it only once while mounting
 
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase(); // FFfffFF => fffffff
-
-    this.setState(() => {
-      return { searchField }; // === return { searchField: filteredMonsters };
-    });
-  };
-
-  render() {
-    // console.log('render');
-
-    //destructuring not to type this. every time:
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-
-    const filteredMonsters = monsters.filter((monster) => {
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
       return monster.name.toLowerCase().includes(searchField);
     });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
 
-    return (
-      <div className='App'>
-        <h1 className='app-title'>Monsters Rolodex</h1>
-        <SearchBox
-          className='monsters-search-box'
-          onChangeHandler={onSearchChange}
-          placeholder='search monsters'
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase(); // FFfffFF => fffffff
+    setSearchField(searchFieldString);
+    // this.setState(() => {
+    //   return { searchField }; // === return { searchField: filteredMonsters };
+    // });
+  };
+
+  return (
+    <div className='App'>
+      <h1 className='app-title'>Monsters Rolodex</h1>
+      <SearchBox
+        className='monsters-search-box'
+        onChangeHandler={onSearchChange}
+        placeholder='search monsters'
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
+
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       monsters: [],
+//       searchField: '',
+//     };
+
+//     // console.log('constructor');
+//   }
+
+//   componentDidMount() {
+//     // console.log('componentDidMount');
+//     fetch('https://jsonplaceholder.typicode.com/users') //promise
+//       .then((response) => response.json())
+//       .then((users) =>
+//         this.setState(
+//           () => {
+//             return { monsters: users };
+//           }
+//           // },
+//           // () => {
+//           //   console.log(this.state);
+//           // }
+//         )
+//       );
+//   }
+
+//   onSearchChange = (event) => {
+//     const searchField = event.target.value.toLocaleLowerCase(); // FFfffFF => fffffff
+
+//     this.setState(() => {
+//       return { searchField }; // === return { searchField: filteredMonsters };
+//     });
+//   };
+
+//   render() {
+//     // console.log('render');
+
+//     //destructuring not to type this. every time:
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+
+//     const filteredMonsters = monsters.filter((monster) => {
+//       return monster.name.toLowerCase().includes(searchField);
+//     });
+
+//     return (
+//       <div className='App'>
+//         <h1 className='app-title'>Monsters Rolodex</h1>
+//         <SearchBox
+//           className='monsters-search-box'
+//           onChangeHandler={onSearchChange}
+//           placeholder='search monsters'
+//         />
+//         <CardList monsters={filteredMonsters} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
